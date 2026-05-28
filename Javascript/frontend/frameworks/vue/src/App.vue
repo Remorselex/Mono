000cf94c33f15819eadff80b1d2ebc50941b0826
remorselex <remorselex99@gmail.com>
2026-05-28 15:41:30 +0200

    feat: cleaned mono root from binary files & wite proj add

---
+++ Javascript/frontend/frameworks/vue/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+  <div>
+    <h1>Vite + Vue + TypeScript</h1>
+    <button @click="count++">count is {{ count }}</button>
+  </div>
+</template>
